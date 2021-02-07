3fa16454ad172e029866e9446f47020a1b83f695
ascobee <austin.scobee@yahoo.com>
2021-02-06 22:08:39 -0700

    Fixed a typo

--- snakegame/snakegame/__main__.py
+++ snakegame/snakegame/__main__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # __main__.py
 
 from .app import play_game
--- snakegame/snakegame/apple.py
+++ snakegame/snakegame/apple.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # apple.py
 
 from random import randint
